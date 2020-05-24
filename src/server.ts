d693d45867dcaa325d3740b4c6a1ff66ced1d344
Lucas C. Soares <lucasc.@Lucass-MacBook-Pro.local>
2020-05-24 14:24:09 -0300

    Separando em módulos

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
-app.use((err: Error, request: Requesr, response: Response, _: NextFunction) => {
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) {
     return response.status(err.statusCode).json({
       status: 'error',
@@ -19,7 +19,6 @@ app.use((err: Error, request: Requesr, response: Response, _: NextFunction) => {
     });
   }
 
-  console.log(err);
   return response.status(500).json({
     status: 'error',
     message: 'internal server error',
